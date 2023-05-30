3a98b2927d103b419258b3d2c8fed06b3cbde407
Myaneee <james.mustacisa7.com>
2023-05-30 23:14:51 +0800

    awda

--- routes/login.js
+++ routes/login.js
@@ -1,63 +1,34 @@
-const connection = require("../dbconnection");
-const express = require("express");
+const connection = require('../dbconnection');
+const express = require('express');
 const router = express.Router();
-//const bcrypt = require('bcrypt');
-/*require('dotenv').config();*/
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const SECRET_KEY = process.env.SECRET_KEY;
 
- router.post("/", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const userprivileges = req.body.userprivileges;
 
-    connection.query(
-        `SELECT * FROM users WHERE username = ? AND password = ?` ,
-        [username, password, userprivileges],
-            (err, result) => {
-        
-            if (err) {
-                res.send({err: err});
-            }
-            if (result.length > 0) {
+router.post('/', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-                res.send(result);
-            }
-            else {
-                res.send({message: "Wrong user/pass"});
-            }
-// //someone is logged in or not
-// router.get("/", (req, res) => {
+  connection.query(
+    'SELECT * FROM users WHERE username = ? AND password = ?',
+    [username, password],
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ error: 'An error occurred during login' });
+      } else {
+        if (result.length > 0) {
 
-//     if(username == null && password == null){ //checks if someone is logged in
-//         res.send("someone is not logged in!")
-//     }
-//     else{
-//         res.send("someone is logged in")
-//     }
-// })
-            
-        /*  
-            //checking password
-            const match = await bcrypt.compare(password, password)
-            if(match){
-                //creating jwts
-                const accessToken = jwt.sign (
-                    {username: username},
-                    process.env.ACCESS_TOKEN_SECRET,
-                    { expiresIn: "30s" }
-                );
-                const refreshToken = jwt.sign (
-                    {username: username},
-                    process.env.REFRESH_TOKEN_SECRET,
-                    { expiresIn: "1d" }
-                );
-                res.json({'success': `User ${username} is logged in! `  })
-            }else{
-                res.sendStatus(401);
-            }
-        */
+          // Generate a JWT token
+          const token = jwt.sign({ username, password }, SECRET_KEY, { expiresIn: '1h' });
+
+          res.json({ token });
+        } else {
+          res.status(401).json({ message: 'Authentication failed' });
         }
-        
-    );
-})
+      }
+    }
+  );
+});
 
 module.exports = router;
