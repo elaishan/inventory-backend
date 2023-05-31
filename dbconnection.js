7a43202b2c89d9518375e5434953549e34e5d435
Kiruu9 <kayerosas20@gmail.com>
2023-05-31 23:21:34 +0800

    c

--- dbconnection.js
+++ dbconnection.js
@@ -4,11 +4,7 @@ const connection = mysql2.createConnection({
     host: 'localhost',
     database: 'inventory',
     user:'root',
-<<<<<<< HEAD
     password:'Abc321',
-=======
-    password:'12345',
->>>>>>> a82821a309ad3b329c2c73d94f7286b5c185407c
 });
 
 connection.connect(function(err){
